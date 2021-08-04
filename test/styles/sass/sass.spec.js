53cb020c91595b2ea5041368993cd5abdf0f47ae
Timo Partti <timo.partti@columbiaroad.com>
2021-08-04 15:35:18 +0300

    Test: Fixed tests

--- test/styles/sass/sass.spec.js
+++ test/styles/sass/sass.spec.js
@@ -4,28 +4,25 @@ const { bundler } = require('../../../dist/lib/bundler');
 const config = { ...global.configDefaults, testPath: __dirname };
 
 describe('SASS/SCSS', () => {
-	test('should create file with index.less', async (done) => {
+	test('should create file with index.less', async () => {
 		await bundler({ ...config, entry: './index.scss' });
 		const content = fs.readFileSync(__dirname + '/.build/index.css', { encoding: 'utf8' });
-		expect(content).toBe('body{background:red}body #ty6p9_wrapper{border:1px solid red}');
-		done();
+		expect(content).toBe('body{background:red}body #t1ux8_wrapper{border:1px solid red}');
 	});
 
-	test('should create file with foo.less', async (done) => {
+	test('should create file with foo.less', async () => {
 		await bundler({ ...config, entry: './foo.sass' });
 		const content = fs.readFileSync(__dirname + '/.build/foo.css', { encoding: 'utf8' });
-		expect(content).toBe('body{background:#00f}body #t1yk6_wrapper{border:1px solid #00f}');
-		done();
+		expect(content).toBe('body{background:blue}body #tm0u5_wrapper{border:1px solid blue}');
 	});
 
-	test('should correct module file', async (done) => {
+	test('should correct module file', async () => {
 		await bundler({ ...config, entry: './index.scss', modules: false });
 		const content = fs.readFileSync(__dirname + '/.build/index.css', { encoding: 'utf8' });
 		expect(content).toBe('body{background:red}body #wrapper{border:1px solid red}');
-		done();
 	});
 
-	test('should correct unminified file', async (done) => {
+	test('should correct unminified file', async () => {
 		await bundler({ ...config, entry: './index.scss', minify: false, modules: false });
 		const content = fs.readFileSync(__dirname + '/.build/index.css', { encoding: 'utf8' });
 		expect(content).toBe(`body {
@@ -33,6 +30,5 @@ describe('SASS/SCSS', () => {
   body #wrapper {
     border: 1px solid red; }
 `);
-		done();
 	});
 });
