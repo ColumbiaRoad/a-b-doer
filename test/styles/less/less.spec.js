c15a7d6bcdbcc7a839a12e19b998fb8568c83d29
Timo Partti <timo.partti@columbiaroad.com>
2021-12-01 12:51:52 +0200

    Test: Fixed tests again

--- test/styles/less/less.spec.js
+++ test/styles/less/less.spec.js
@@ -7,19 +7,19 @@ describe('LESS', () => {
 	test('should create file with index.less', async () => {
 		await bundler({ ...config, entry: './index.less' });
 		const content = fs.readFileSync(__dirname + '/.build/index.css', { encoding: 'utf8' });
-		expect(content).toBe('body{background:red}body #t1m9j-wrapper{border:1px solid red}');
+		expect(content).toMatch(/body\{background:(#f00|red)\}body #t1m9j-wrapper\{border:1px solid (#f00|red)\}/);
 	});
 
 	test('should create file with foo.less', async () => {
 		await bundler({ ...config, entry: './foo.less' });
 		const content = fs.readFileSync(__dirname + '/.build/foo.css', { encoding: 'utf8' });
-		expect(content).toBe('body{background:#00f}body #t1afo-wrapper{border:1px solid #00f}');
+		expect(content).toMatch(/body\{background:(#00f|blue)\}body #t1afo-wrapper\{border:1px solid (#00f|blue)\}/);
 	});
 
 	test('should correct module file', async () => {
 		await bundler({ ...config, entry: './index.less', modules: false });
 		const content = fs.readFileSync(__dirname + '/.build/index.css', { encoding: 'utf8' });
-		expect(content).toBe('body{background:red}body #wrapper{border:1px solid red}');
+		expect(content).toMatch(/body\{background:(#f00|red)\}body #wrapper\{border:1px solid (#f00|red)\}/);
 	});
 
 	test('should correct unminified file', async () => {
