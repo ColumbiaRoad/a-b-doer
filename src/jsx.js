fc795ae13517708d94e56841bdd72c84b640d845
Timo Partti <timo.partti@columbiaroad.com>
2021-12-03 17:08:33 +0200

    Removed old comment

--- src/jsx.js
+++ src/jsx.js
@@ -5,9 +5,6 @@ import { Fragment } from './utils/render';
 /**
  * Simple parser utility for jsx syntax. Converts JSX syntax to simple objects that can be rendered to DOM nodes with render utility (@see render() [./utils/render.js])
  * Should be used with babel and plugin-transform-react-jsx.
- *
- * If a state or hooks or any other react related thing is required, I would recommend using the preact library which is
- * a little bit larger (but still very small) lib but handles nicely all more advanced stuff (test config option preact: true).
  */
 
 /**
