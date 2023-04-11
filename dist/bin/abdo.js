fd0bd6ae5b0408b344a24a622e0c6df504ce739d
Timo Partti <timo.partti@columbiaroad.com>
2023-04-11 09:54:44 +0300

    fix: use plugin helper from utils

--- dist/bin/abdo.js
+++ dist/bin/abdo.js
@@ -1328,12 +1328,12 @@ async function bundler(buildSpecConfig) {
 		assetBundle = { js: true, bundle: injection };
 
 		try {
-			await openPage({ ...testConfig, assetBundle, bundlerConfig }, true);
+			await openPage({ ...testConfig, assetBundle }, true);
 		} catch (error) {
 			console.log('Error while opening page', error);
 		}
 	}
-	return { ...testConfig, assetBundle, server, bundlerConfig };
+	return { ...testConfig, assetBundle, server };
 }
 
 /**
